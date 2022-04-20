45f8dd351eb88c52361845699e4a9b728403b284
mai <fujisawamai21@gmail.com>
2022-04-20 11:02:38 +0900

    Contactクラスの値を初期化するようにした。

--- ex01/Contact.cpp
+++ ex01/Contact.cpp
@@ -1,6 +1,11 @@
 #include "Contact.hpp"
 Contact::Contact()
 {
+    first_name = "";
+    last_name = "";
+    nickname = "";
+    phone_number = "";
+    darkest_secret = "";
 }
 
 Contact::~Contact()
